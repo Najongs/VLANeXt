1231594a80907a809f041a73bd3303bb60d2887d
nayohan <nayohan13@gmail.com>
2026-03-20 01:12:01 +0900

    260320

--- Run_Eval.sh
+++ Run_Eval.sh
@@ -1,4 +1,8 @@
-unset PYTHONPATH
-export PYTHONPATH=$PYTHONPATH:~/proj/VLANeXt/third_party/LIBERO
+# unset PYTHONPATH
+# export PYTHONPATH=$PYTHONPATH:~/proj/VLANeXt/third_party/LIBERO
 
-CUDA_VISIBLE_DEVICES=0 MUJOCO_EGL_DEVICE_ID=0 python -m scripts.libero_bench_eval
+# CUDA_VISIBLE_DEVICES=0 MUJOCO_EGL_DEVICE_ID=0 python -m scripts.libero_bench_eval
+
+python scripts/sim_eval.py \
+    --config config/sim_eval_config.yaml \
+    --checkpoint checkpoints/VLANeXt_droid.pt
