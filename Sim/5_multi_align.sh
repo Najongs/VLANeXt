8aaf01d3965988472df9ec02b70409198817a606
nayohan <nayohan13@gmail.com>
2026-04-30 14:33:51 +0900

    260430

--- Sim/5_multi_align.sh
+++ Sim/5_multi_align.sh
@@ -1,19 +1,19 @@
-python run_parallel.py --script align --workers 20 --episodes 50 \
-    --base-dir /data/public/NAS/VLANeXt/dataset/fine_align/fine_align_00 --phantom-pos 0.0 0.0 --no-side-camera --cameras tool_camera
+# python run_parallel.py --script align --workers 20 --episodes 50 \
+#     --base-dir /data/public/NAS/VLANeXt/dataset/fine_align/fine_align_00 --phantom-pos 0.0 0.0 --no-side-camera --cameras tool_camera
 # python run_parallel.py --script align --workers 20 --episodes 50 \
 #     --base-dir /data/public/NAS/VLANeXt/dataset/fine_align/fine_align_01 --phantom-pos 0.0 -0.1 --no-side-camera --cameras tool_camera
 # python run_parallel.py --script align --workers 20 --episodes 50 \
 #     --base-dir /data/public/NAS/VLANeXt/dataset/fine_align/fine_align_03 --phantom-pos 0.0 -0.3 --no-side-camera --cameras tool_camera
-# python run_parallel.py --script align --workers 20 --episodes 50 \
-#     --base-dir /data/public/NAS/VLANeXt/dataset/fine_align/fine_align_04 --phantom-pos 0.0 -0.4 --no-side-camera --cameras tool_camera
+python run_parallel.py --script align --workers 20 --episodes 500 \
+    --base-dir /data/public/NAS/VLANeXt/dataset/fine_align/fine_align_04 --phantom-pos 0.0 -0.4 --no-side-camera --cameras tool_camera
 
 # python Save_dataset_align_only.py --save-dir /data/public/NAS/VLANeXt/Sim/test_xyz --perturb -30 30 -30
 
 # python run_parallel.py --script align --workers 1 --episodes 1 \
 #     --base-dir /data/public/NAS/VLANeXt/dataset/fine_align/fine_align_02 --phantom-pos 0.0 -0.2 --no-side-camera
 
-python run_parallel.py --script approach --workers 10 --episodes 10 \
-    --base-dir /data/public/NAS/VLANeXt/dataset/approach/approach_00 --phantom-pos 0.0 -0.0 --no-side-camera --no-insertion --hold-steps 25 --cameras top_camera
+# python run_parallel.py --script approach --workers 10 --episodes 10 \
+#     --base-dir /data/public/NAS/VLANeXt/dataset/approach/approach_00 --phantom-pos 0.0 -0.0 --no-side-camera --no-insertion --hold-steps 25 --cameras top_camera
 # python run_parallel.py --script approach --workers 1 --episodes 5 \
 #     --base-dir /data/public/NAS/VLANeXt/dataset/approach/approach_01 --phantom-pos 0.0 -0.1 --no-side-camera --no-insertion --hold-steps 25 --cameras top_camera
 # python run_parallel.py --script approach --workers 20 --episodes 250 \
